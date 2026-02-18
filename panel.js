c0f35f610ab97022b42dec048c8fff895d6b577b
BooyahBlitz FF <baibhawkumar975111@gmail.com>
2026-02-18 07:05:50 +0000

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,15 +1,27 @@
-const user = localStorage.getItem("loggedInUser");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth,onAuthStateChanged,signOut }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+const firebaseConfig = {
+apiKey: "AIzaSyD0bAbqftvWt5jd1k1dOFcR6oeBZ_qwjwY",
+authDomain: "lunarhost-923f6.firebaseapp.com",
+projectId: "lunarhost-923f6",
+storageBucket: "lunarhost-923f6.firebasestorage.app",
+messagingSenderId: "245861345265",
+appId: "1:245861345265:web:c08f2c973d4d5d38f68650"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// WAIT for Firebase session
+onAuthStateChanged(auth,user=>{
 if(!user){
 location.href="index.html";
 }
-
-function logout(){
-localStorage.removeItem("loggedInUser");
-location.href="index.html";
-}
-
-document.addEventListener("DOMContentLoaded", ()=>{
-let u = document.getElementById("usernameDisplay");
-if(u) u.innerText = user;
 });
+
+// logout
+window.logout=()=>{
+signOut(auth).then(()=>location.href="index.html");
+};
